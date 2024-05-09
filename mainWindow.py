e142d461e71f8777dc779ac81701c7c84a70ac77
ocharek <oskarjasiak@gmail.com>
2024-05-09 12:42:20 +0200

    Adding Path to save

--- mainWindow.py
+++ mainWindow.py
@@ -1,5 +1,5 @@
-from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QTextEdit
-from PySide6.QtGui import QTextOption
+from PySide6.QtWidgets import QWidget, QLabel, QVBoxLayout, QPushButton, QTextEdit, QFileDialog, QGridLayout, QCheckBox
+from PySide6.QtGui import QTextOption, QIcon
 from PySide6.QtCore import Qt
 
 import qrCreate
@@ -12,26 +12,66 @@ class MainWindow(QWidget):
     def initUI(self):
         # Create items
         # Label
-        lab = QLabel('What should QR code contain?')
-        # Managing text field
+        lab1 = QLabel('What should QR code contain?')
+        # Managing value text field
         self.link = SingleLineTextEdit(self)
         self.link.setWordWrapMode(QTextOption.WrapMode.NoWrap)
         self.link.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
-        self.link.setFixedSize(200, 26)
-        # Button
-        crbut = QPushButton("Make It QR!")
-        crbut.clicked.connect(self.makeQR)
+        self.link.setFixedSize(220, 26)
+        # Managing directory path text field and explorer button
+        self.dir = SingleLineTextEdit(self)
+        self.dir.setWordWrapMode(QTextOption.WrapMode.NoWrap)
+        self.dir.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAlwaysOff)
+        self.dir.setEnabled(False)
+        self.dir.setFixedSize(170, 26)
+        self.dir.setStyleSheet("color: gray")
+        self.dir.setPlaceholderText("Path where u want(if u want) save QR")
+
+        self.pathbut = QPushButton(self)
+        self.pathbut.setToolTip('Explore directories')
+        self.pathbut.setFixedSize(20, 20)
+        # pathicon = QIcon("/IMG/file-explorer.png")
+        # pathico = pathicon.pixmap(10, 10)
+        # self.pathbut.setIcon(pathico)
+        self.pathbox = QCheckBox(self)
+        self.pathbox.setStyleSheet("QCheckBox::indicator { width: 20px; height: 20px; }")
+        self.pathbox.stateChanged.connect(self.onPathCheckbox)
+        # self.button.clicked.connect(self.openDirectoryDialog)
+        # Creating button
+        self.crbut = QPushButton("Make It QR!")
+        self.crbut.clicked.connect(self.makeQR)
         # Taking value from text field
         # Create layout and adding items
-        layout = QVBoxLayout()
-        layout.addWidget(lab)
-        layout.addWidget(self.link)
-        layout.addWidget(crbut)
+        layout = QGridLayout()
+        layout.addWidget(lab1, 0, 0, 1, 3)
+        layout.addWidget(self.link, 1, 0, 1, 3)
+        layout.addWidget(self.dir, 2, 0)
+        layout.addWidget(self.pathbut, 2, 1)
+        layout.addWidget(self.pathbox)
+        layout.addWidget(self.crbut, 3, 0, 1, 3)
         self.setLayout(layout)
         self.setWindowTitle("QR Creator")
         self.show()
         self.activateWindow()
+        # Setting start pos and size of window
         self.setGeometry(100, 100, 300, 200)
+        # Setting maximum size
+        self.setMaximumHeight(100)
+        self.setMaximumWidth(100)
+
+    # def openDirectoryDialog(self):
+    #     # Show the directory dialog and get the selected directory
+    #     directory_dialog = QFileDialog(self)
+    #     directory_path = directory_dialog.getExistingDirectory(self, "Open Directory", "")
+    #
+    #     if directory_path:
+    #         print("Selected directory:", directory_path)
+
+    def onPathCheckbox(self, state):
+        if state == 2:
+            self.dir.setEnabled(True)
+        else:
+            self.dir.setEnabled(False)
 
     def makeQR(self):
         dat = self.link.toPlainText()
